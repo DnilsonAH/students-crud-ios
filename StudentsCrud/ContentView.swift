89d0aeacaf6c5d298f34c9cb99d94d59d06dcf48
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-05-14 15:04:42 -0500

    Initial Commit

---
+++ StudentsCrud/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StudentsCrud
+//
+//  Created by Dnilson Achahuanco on 14/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudentsCrud/StudentsCrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentsCrudApp.swift
+//  StudentsCrud
+//
+//  Created by Dnilson Achahuanco on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct StudentsCrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
